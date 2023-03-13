505518ad4bbd942b7c76ab30036a71aa0f9db7a2
ToTuanAn <totuanan06@gmail.com>
2023-03-13 14:36:43 +0700

    base

---
+++ NachOS-4.0/code/test/createfile.c
@@ -0,0 +1,14 @@
+#include "syscall.h"
+#define MAX_LENGTH 32
+
+int main()
+{   
+    // int stdin;
+	// char fileName[MAX_LENGTH];
+	// PrintString("\n\t\t\t-----TAO FILE-----\n\n");
+	// PrintString(" - Nhap ten file tu stdin (Open(stdin)): ");
+
+
+    Create("hello.txt");
+    return 0;
+}
